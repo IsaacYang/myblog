d373637f28d1092b96e54fa17afbfb024cb47131
isaac <849611785@qq.com>
2020-11-18 16:09:18 +0800

    added rss feeds

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from .feeds import LatestPostsFeed
 
 app_name='blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
 ]
